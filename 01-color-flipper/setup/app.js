40532c6c43089dfe751f3a25b77b8bae914f3ee8
myunusyesil <mehmetyunus@windowslive.com>
2022-07-06 14:56:24 +0300

    bgcolor shifter simple

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,2 +1,21 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-// deneme 
+
+/* 
+First we need to select button and background color value and 
+than addeventlistener to the click me button and manipulate background accordingly
+
+*/
+
+const btn = document.getElementById('btn');
+const value = document.querySelector('.color');
+
+btn.addEventListener ('click', function() {
+    var random = randomNum();
+    document.body.style.backgroundColor = colors[random];
+    value.textContent = colors[random];
+});
+
+/* Generating random number between 0-4 and floor it nearest integer*/
+function randomNum () {
+    return Math.floor(Math.random()*4);
+}

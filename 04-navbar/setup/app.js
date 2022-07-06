926efc07c843ca183207eaf716648fffb24072f5
myunusyesil <mehmetyunus@windowslive.com>
2022-07-06 17:59:20 +0300

    toggle bar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// Select menu icon and links class 
+
+const toggleBtn = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links')
+
+toggleBtn.addEventListener('click', function(){
+    console.log(toggleBtn, links);
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links');
+    // }
+    // else {
+    //     links.classList.add('show-links');
+    // }
+
+        links.classList.toggle ('show-links')
+
+});

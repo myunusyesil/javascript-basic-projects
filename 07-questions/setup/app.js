81a5b2bb1be9ff7f55ab4060fc413601ee183582
myunusyesil <mehmetyunus@windowslive.com>
2022-07-07 14:34:35 +0300

    question section done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,40 @@
 //using selectors inside the element
+/* Bu yönetimin anlaşılması biraz daha zor. Bu bölümde butona basıldığında
+    diğer açık olanların kapatılması konusunu anlamadım.
+    Temel olarak doğrudan elamanı seçtik. Bütün question'ları
+    Bu questionları forEach ile döndük ve gönderdiğimiz parametre aracılığıyla
+    butonunu aldık. sonra bu button'ları eventlistener ile click dinledik
+
+*/
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach( function(question){
+    const btn = question.querySelector('.question-btn')
+    // console.log(btn);
+    btn.addEventListener('click', function(){
+        questions.forEach(function(item){
+            // console.log(item);
+            if (item != question) {
+                console.log("item:", item, "question:", question);
+                item.classList.remove('show-text');
+            }
+        })
+        question.classList.toggle('show-text');
+     });
+    // console.log(btn);
+});
+
+
 // traversing the dom
+// const questionBtn = document.querySelectorAll('.question-btn');
+// console.log(questionBtn);
+
+// questionBtn.forEach( function(btn) {
+//     btn.addEventListener('click', function(e){
+//         var question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//         console.log(question);
+//     });
+// });
+

62ea6d083fbf07b449d8841d5648438ae479f9d2
myunusyesil <mehmetyunus@windowslive.com>
2022-07-16 14:40:57 +0300

    scroll project date-toggle-fixedbar-backtotop fea

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -6,12 +6,52 @@
 // ********** set date ************
 const date = document.getElementById('date');
 const currentYear = new Date().getFullYear();
-console.log(date, currentYear);
+// console.log(date, currentYear);
 date.innerHTML = currentYear;
 
 // ********** close links ************
+const toggleBtn = document.querySelector(".nav-toggle");
+const linkContainer = document.querySelector(".link-container");
+const links = document.querySelector(".links");
+// console.log(links);
+
+toggleBtn.addEventListener('click', function() {
+    // linkContainer.classList.toggle('show-links');
+    // calculate linksheight
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linkContainer.getBoundingClientRect().height;
+    console.log(linksHeight); 
+
+    if ( containerHeight === 0) {
+        linkContainer.style.height = `${linksHeight}px`;
+    } else {
+        linkContainer.style.height = 0 ;
+    }
+});
 
 // ********** fixed navbar ************
+
+    window.addEventListener('scroll', function () {
+        const navBar = document.getElementById("nav");
+        const topLink = document.querySelector(".top-link");
+        let navHeight = navBar.getBoundingClientRect().height;
+        console.log(navBar, navHeight);
+        // console.log(pageYOffset); 
+        let positionScroll = window.pageYOffset;
+        if ( positionScroll > navHeight  ) {
+            navBar.classList.add("fixed-nav")
+        }
+        else {
+            navBar.classList.remove("fixed-nav")
+        }
+        // back to bottom function
+        if ( positionScroll > 500) {
+            topLink.classList.add("show-link");
+        }
+        else {
+            topLink.classList.remove("show-link");
+        }
+    })
 
 // ********** smooth scroll ************
 // select links
